523e306be208f5265acf79bb584884228841f173
Mirco Cotugno <mirco@pitchtarget.com>
2020-09-16 11:03:08 +0200

    delete ButtonNav

--- frontend/src/routes/StudentsPage.js
+++ frontend/src/routes/StudentsPage.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
-import { Grid } from "@material-ui/core";
-import ButtonNavBar from "../components/ButtonNavBar";
+import { Grid, Button } from "@material-ui/core";
 
 export default function StudentsPage() {
   const [openLateralView, setOpenLateralView] = useState(false);
 
   return (
     <>
-      <Grid container direction="row" alignItems="center" justify="space-between" >
-        <Grid item >  <h1>Students Page</h1> </Grid>
-        <Grid item >
-        <ButtonNavBar onClick={() => setOpenLateralView(true)}>{/*  */}
-        NEW STUDENT
-  </ButtonNavBar>  </Grid>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justify="space-between"
+      >
+        <Grid item>
+          <h1>Students Page</h1>
+        </Grid>
+        <Grid item>
+          <Button className="custom-button">new student</Button>
+        </Grid>
       </Grid>
     </>
   );
 }
-
-
